93dbb6243af30dacf82d3e2046d6fd020405b127
thurose18 <thudangrose@gmail.com>
2025-06-25 10:04:46 +0700

    Them vat pham thu thap

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,13 +1,29 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public bool isGameOver = false;
 
+    public int score = 0;
+    public Text scoreText; // Kéo UI Text vào đây trong Inspector
+
+
     void Awake()
     {
         Instance = this;
+    }
+    public void AddScore(int value)
+    {
+        score += value;
+        UpdateScoreUI();
+    }
+
+    void UpdateScoreUI()
+    {
+        if (scoreText != null)
+            scoreText.text = "Score: " + score;
     }
 
     public void GameOver()
---
+++ Assets/Star.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Tăng điểm
+            if (GameManager.Instance != null)
+                GameManager.Instance.AddScore(1);
+
+            // Ẩn vật phẩm (hoặc Destroy nếu không dùng pooling)
+            gameObject.SetActive(false);
+        }
+    }
+}
