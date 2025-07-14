db3eb32e537ee08379f14744f894fbc4ebe14344
thurose18 <thudangrose@gmail.com>
2025-07-14 14:34:55 +0700

    Update GameOverUI, add PlayAgain

--- Assets/GameOverUI.cs
+++ Assets/GameOverUI.cs
@@ -1,21 +1,55 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;  
 
 public class GameOverUI : MonoBehaviour
 {
     public static GameOverUI Instance;
     public GameObject gameOverPanel;
+    public Text restartHintText;
+
+    private bool canRestart = false;
 
     void Awake()
     {
         Instance = this;
-        if (gameOverPanel != null)
-            gameOverPanel.SetActive(false);
+        gameOverPanel.SetActive(false);
+        if (restartHintText != null)
+            restartHintText.gameObject.SetActive(false);
     }
 
     public void Show()
     {
-        if (gameOverPanel != null)
-            gameOverPanel.SetActive(true);
+        gameOverPanel.SetActive(true);
+        if (restartHintText != null)
+        {
+            restartHintText.text = "Nhấn vào màn hình để chơi lại";
+            restartHintText.gameObject.SetActive(true);
+        }
+        canRestart = true;
         Debug.Log("Game Over UI is now visible!");
+    }
+    void Update()
+    {
+        if (canRestart)
+        {
+#if UNITY_EDITOR
+            if (Input.GetMouseButtonDown(0))
+            {
+                RestartGame();
+            }
+#else
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                RestartGame();
+            }
+#endif
+        }
+    }
+
+    void RestartGame()
+    {
+        canRestart = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
